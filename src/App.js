9932a5d0ce865a85876ca0a035e01fd025e7cd22
Mohammed Anshad <85294714+anshadkonnola@users.noreply.github.com>
2023-04-26 20:28:56 +0530

    feat:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [query, setQuery] = useState('');
+
+
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    try {
+      let link = query;
+      if(link.slice(0, 4) === 'www.') link = `https://${link}`;
+      let url = new URL(link);
+      window.location.href = link;
+    } catch(_){
+      window.location.href = `https://www.google.com/search?q=${query}`;
+    }
+  };
+
+  const handleLucky = (event) => {
+    event.preventDefault();
+    window.location.href = `https://www.google.com/search?q=${query}&btnI=`;
+  };
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  document.title = 'Google';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="links">
+          <a href="https://mail.google.com/mail/?authuser=0&ogbl">Gmail</a>
+          <a href="https://www.google.co.in/imghp?hl=en&authuser=0&ogbl">Images</a>
+          <a href="https://www.google.co.in/intl/en/about/products">Apps</a>
+          <a href="https://accounts.google.com/SignOutOptions?hl=en&continue=https://www.google.com/">Account</a>
+        </div>
+      </div>
+      <div className="logo">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" />
+      </div>
+      <form className="search">
+        <input type="text" className="search-input" value={query} onChange={handleInputChange} />
+      </form>
+      <div className="search-buttons">
+          <button className="search-btn" onClick={handleSearch}>Google Search</button>
+          <button className="feeling-lucky-btn" onClick={handleLucky}>I'm Feeling Lucky</button>
+      </div>
     </div>
   );
 }
